f8a116ced61697e73d755cb68649a23e6291a330
Amir Kiumarsi <codacy20@users.noreply.github.com>
2020-08-21 22:17:34 +0200

    ui look and feel

--- src/links/links.tsx
+++ src/links/links.tsx
@@ -5,37 +5,45 @@ export class Links extends Component<{}, {}> {
   render() {
     return (
       <div className="container-links">
-        <h4>Documention</h4>
-        <span>
-          Various versions have evolved over the years, sometimes by accident
-        </span>
-
-        <div>
-          <span>Set up</span>
-          <span className="material-icons">keyboard_arrow_right</span>
+        <div className="header">
+          <h4>Documention</h4>
+          <div id="header-detail">
+            <span>
+              Various versions have evolved over the years, sometimes by
+              accident
+            </span>
+          </div>
         </div>
 
-        <div>
-          <span>Set up</span>
-          <span className="material-icons">keyboard_arrow_right</span>
-        </div>
+        <div className="wrapper-links">
+          <div className="links">
+            <span>Set up</span>
+            <span className="material-icons">keyboard_arrow_right</span>
+          </div>
 
-        <div>
-          <span>Set up</span>
-          <span className="material-icons">keyboard_arrow_right</span>
-        </div>
+          <div className="links">
+            <span>Set up</span>
+            <span className="material-icons">keyboard_arrow_right</span>
+          </div>
 
-        <div>
-          <span>Set up</span>
-          <span className="material-icons">keyboard_arrow_right</span>
-        </div>
+          <div className="links">
+            <span>Set up</span>
+            <span className="material-icons">keyboard_arrow_right</span>
+          </div>
 
-        <div>
-          <span>Set up</span>
-          <span className="material-icons">keyboard_arrow_right</span>
-        </div>
+          <div className="links">
+            <span>Set up</span>
+            <span className="material-icons">keyboard_arrow_right</span>
+          </div>
 
-        <a href="#">View our courses</a>
+          <div className="links">
+            <span>Set up</span>
+            <span className="material-icons">keyboard_arrow_right</span>
+          </div>
+        </div>
+        <div id="view-course">
+          <a href="#">View our courses</a>
+        </div>
       </div>
     );
   }
