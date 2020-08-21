0aecb88640633b65e059be86076aab6bdf661deb
Amir Kiumarsi <codacy20@users.noreply.github.com>
2020-08-21 14:04:02 +0200

    tiles style

--- src/links/links.tsx
+++ src/links/links.tsx
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import "./links.scss";
 
 export class Links extends Component<{}, {}> {
   render() {
     return (
-      <div>
-        <p>your daddy</p>
+      <div className="container-links">
+        <h4>Documention</h4>
+        <span>
+          Various versions have evolved over the years, sometimes by accident
+        </span>
+
+        <div>
+          <span>Set up</span>
+          <span className="material-icons">keyboard_arrow_right</span>
+        </div>
+
+        <div>
+          <span>Set up</span>
+          <span className="material-icons">keyboard_arrow_right</span>
+        </div>
+
+        <div>
+          <span>Set up</span>
+          <span className="material-icons">keyboard_arrow_right</span>
+        </div>
+
+        <div>
+          <span>Set up</span>
+          <span className="material-icons">keyboard_arrow_right</span>
+        </div>
+
+        <div>
+          <span>Set up</span>
+          <span className="material-icons">keyboard_arrow_right</span>
+        </div>
+
+        <a href="#">View our courses</a>
       </div>
     );
   }
--- src/tiles/tiles.tsx
+++ src/tiles/tiles.tsx
@@ -1,10 +1,51 @@
 import React, { Component } from "react";
+import "./tiles.scss";
 
 export class Tiles extends Component<{}, {}> {
   render() {
     return (
-      <div>
-        <p>your daddy</p>
+      <div className="container-tiles">
+        <h4>Get Started</h4>
+        <div className="divider"></div>
+        <div className="tile">
+          <div className="header">
+            <span className="material-icons">credit_card</span>
+            <h4>title</h4>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+          </p>
+        </div>
+
+        <div className="tile">
+          <div className="header">
+            <span className="material-icons">menu_book</span>
+            <h4>title</h4>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+          </p>
+        </div>
+
+        <div className="tile">
+          <div className="header">
+            <span className="material-icons">extension</span>
+            <h4>title</h4>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+          </p>
+        </div>
+
+        <div className="tile">
+          <div className="header">
+            <span className="material-icons">code</span>
+            <h4>title</h4>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+          </p>
+        </div>
       </div>
     );
   }
